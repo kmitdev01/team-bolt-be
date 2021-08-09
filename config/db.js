e864945d184c6d24752f7b6183c562a444cadaad
gaurav tomar <gauravt@keymouseit.com>
2021-08-09 12:23:59 +0530

    change local database to mongoatlas db

--- config/db.js
+++ config/db.js
@@ -1,4 +1,21 @@
-const mongoose = require("mongoose");
-mongoose.connect('mongodb://localhost:27017/teambolt', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
+//const mongoose = require("mongoose");
+//mongoose.connect('mongodb://localhost:27017/teambolt', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
+
+ const mongoose = require('mongoose')
+
+const url = 'mongodb+srv://teambolt:Admin@123@cluster0.gpi0b.mongodb.net/teambolt-db?retryWrites=true&w=majority';
+
+const connectionParams = {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+	}
+ mongoose.connect(url, connectionParams)
+    .then(() => {
+        console.log('Connected to database ')
+    })
+    .catch((err) => {
+        console.error(`Error connecting to the database. \n${err}`);
+    })
 
 module.exports = mongoose;
